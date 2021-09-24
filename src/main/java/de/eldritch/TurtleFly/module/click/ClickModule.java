ae75671ff19b395bd9f66f9309bdcb38cf472590
TurtleException <turtle.exception@eldritch.de>
2021-09-24 02:52:20 +0200

    Add ClickModule

--- src/main/java/de/eldritch/TurtleFly/module/click/ClickModule.java
+++ src/main/java/de/eldritch/TurtleFly/module/click/ClickModule.java
@@ -1,7 +1,16 @@
 package de.eldritch.TurtleFly.module.click;
 
+import de.eldritch.TurtleFly.Plugin;
 import de.eldritch.TurtleFly.module.PluginModule;
 import de.eldritch.TurtleFly.module.PluginModuleEnableException;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Particle;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 /**
  * Allows custom interactions when players click on each other.
@@ -11,5 +20,26 @@ public class ClickModule extends PluginModule {
         super();
     }
 
-    // TODO
+
+    private void registerListeners() {
+        Plugin.getPlugin().getServer().getPluginManager().registerEvents(new Listener() {
+            @EventHandler
+            public void onEvent(PlayerInteractEntityEvent event) {
+                if (event.getPlayer().getInventory().getItem(event.getHand()).getType().isAir()) {
+                    onClick(event.getPlayer(), event.getRightClicked());
+                }
+            }
+        }, Plugin.getPlugin());
+    }
+
+    public void onClick(Player source, Entity target) {
+        String message = getConfig().getString(source.getUniqueId() + "." + target.getUniqueId() + ".message", null);
+        Particle particle = getConfig().getObject(source.getUniqueId() + "." + target.getUniqueId() + ".particle", Particle.class, null);
+
+        if (particle != null) {
+            if (message != null && target instanceof Player)
+                ((Player) target).spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent("§c" + message));
+            target.getWorld().spawnParticle(particle, target.getLocation().add(0, 1, 0), 10, 0.5, 0.5, 0.5);
+        }
+    }
 }
