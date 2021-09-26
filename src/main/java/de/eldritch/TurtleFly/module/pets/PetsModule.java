43f9fd2a50913096e514bf54630f952eab5cce48
TurtleException <turtle.exception@eldritch.de>
2021-09-26 02:04:50 +0200

    Update PetsModule

--- src/main/java/de/eldritch/TurtleFly/module/pets/PetsModule.java
+++ src/main/java/de/eldritch/TurtleFly/module/pets/PetsModule.java
@@ -1,7 +1,13 @@
 package de.eldritch.TurtleFly.module.pets;
 
+import de.eldritch.TurtleFly.Plugin;
 import de.eldritch.TurtleFly.module.PluginModule;
 import de.eldritch.TurtleFly.module.PluginModuleEnableException;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Tameable;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 
 /**
  * Allows players to share pets.
@@ -9,7 +15,22 @@ import de.eldritch.TurtleFly.module.PluginModuleEnableException;
 public class PetsModule extends PluginModule {
     public PetsModule() throws PluginModuleEnableException {
         super();
+
+        this.registerListeners();
     }
 
-    // TODO
+    private void registerListeners() {
+        Plugin.getPlugin().getServer().getPluginManager().registerEvents(new Listener() {
+            @EventHandler
+            public void onEvent(PlayerInteractAtEntityEvent event) {
+                if (event.getRightClicked() instanceof Tameable) {
+                    onClick(event.getPlayer(), (Tameable) event.getRightClicked());
+                }
+            }
+        }, Plugin.getPlugin());
+    }
+
+    public void onClick(Player source, Tameable pet) {
+        // TODO
+    }
 }
